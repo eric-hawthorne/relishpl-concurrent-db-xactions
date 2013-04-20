18237a524ab808cf02d687a3c67044d5c05e8a27
Eric Hawthorne <relishpl@gmail.com>
2013-04-20 12:04:11 -0700

    interim commit

---
+++ src/relish/runtime/native_types/init_native_types.go
@@ -0,0 +1,28 @@
+// Copyright 2012-2013 EveryBitCounts Software Services Inc. All rights reserved.
+// Use of this source code is governed by the GNU LESSER GPL v3 license, found in the LICENSE_LGPL3 file.
+
+package native_types
+
+/*
+   init_native_types.go 
+*/
+
+
+/*
+IMPORTANT
+
+If you add a type whose instances are native-Go-implemented, you must add the fully qualified type name to 
+this map, so that the RType object will configure itself as having .IsNative = true
+This will cause a GoWrapper object to be instantiated upon a constructor call for the type. 
+You must then implement a native Go constructor function in a .go file in 
+the approprate native_methods/extensions package, to actually construct and initialize the Go object instance.
+*/
+var NativeType = map [string] bool {
+	
+	// relish standard library types with Go native instances.
+	// DO NOT MODIFY THIS LIST OF MAP ENTRIES !!
+	"relish.pl2012/relish_lib/pkg/files/File" : true,
+	
+	// OK TO MODIFY THE ENTRIES FROM HERE DOWN !!
+	// Add extensions types which need a GoWrapper as the instance here. 
+}
