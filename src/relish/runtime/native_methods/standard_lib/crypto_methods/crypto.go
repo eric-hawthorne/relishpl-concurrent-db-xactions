1efa0474570c17e07886f1bd6c195b13ade95b46
Eric Hawthorne <relishpl@gmail.com>
2013-10-17 07:04:14 -0700

    broken encryption - needs go update

--- src/relish/runtime/native_methods/standard_lib/crypto_methods/crypto.go
+++ src/relish/runtime/native_methods/standard_lib/crypto_methods/crypto.go
@@ -47,7 +47,7 @@ func InitCryptoMethods() {
 // Stereotypical Crypto functions including public-key crypto key-pair generation, 
 // digital-object signing, and signature verification.
 
-// generateKeyPair keyLenBits Int passwordForPrivateKey String > privateKeyPEM String publicKeyPEM String err String
+// generateKeyPair keyLenBits Int passwordForPrivateKey String > privateKeyPem String publicKeyPem String err String
 // """
 //  Generates an RSA private key and public key as ascii-armoured (base-64) PEM strings.
 //  Minimum keyLenBits recommended is 2048, 3072 is better.
@@ -66,6 +66,33 @@ func generateKeyPair (th InterpreterThread, objects []RObject) []RObject {
     }
 	return []RObject{String(privateKeyPEM), String(publicKeyPEM), String(errStr)}    
 }
+
+/*
+sign privateKeyPem String privateKeyPassword String content String > signaturePem String err String
+
+Signs based on SHA256 hash of the content.
+*/
+func Sign (th InterpreterThread, objects []RObject) []RObject {
+    privateKeyPEM := string(objects[0].(String))
+    password := string(objects[1].(String))
+    content := string(objects[2].(String))
+    signaturePEM, err := crypto_util.Sign(privateKeyPEM, password, content) 
+	
+    var errStr string
+    if err != nil {
+       errStr = err.Error()
+    }
+	return []RObject{String(signaturePEM), String(errStr)}    
+}
+
+
+/*
+Verifies the signature as being the signature for the content.
+Returns true if the signature is the signature of the content, as signed by the private key
+that corresponds to the argument public key.
+Assumes that SHA256 was used as the hash function to hash the content for signing.
+*/
+func Verify(publicKeyPEM string, signaturePEM string, content string) bool {
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
