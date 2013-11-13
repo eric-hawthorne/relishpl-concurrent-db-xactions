9f53cb0af9db064501aae09411db6b41a84a54f2
Eric Hawthorne <relishpl@gmail.com>
2013-11-12 16:34:34 -0800

    starting to add rtPath method

--- src/relish/defs/defs.go
+++ src/relish/defs/defs.go
@@ -19,6 +19,7 @@ var StandardLibPackageArtifact map[string]string = map[string]string {
 	"http" : "relish",
 	"csv" : "relish",	
 	"json" : "relish",	
+	"env" : "relish",	
 	"io" : "shared.relish.pl2012/relish_lib",		
 	"files" : "shared.relish.pl2012/relish_lib",	
 	"http_srv" : "shared.relish.pl2012/relish_lib",		
