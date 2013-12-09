04b24a93fc5033b37b6c1a7044e849d51e1370b1
Eric Hawthorne <relishpl@gmail.com>
2013-12-08 17:00:48 -0800

    fixed up install script and instructions

--- install.sh
+++ install.sh
@@ -8,25 +8,32 @@
 #
 # For installation of a relish binary distribution:
 #
-# cd /opt/relish
+# cd /opt/relish or cd ~/relish
 # sudo ./install.sh
 #
 # Or for installation of a relish source distribution:
 #
-# cd /opt/relish
+# cd /opt/devel/relish
 # sudo ./install.sh
 #
 # Make relish command available in default PATH 
 # and make sure relish directory tree is writeable and commands are executable.
 #
-mkdir -p /usr/local/bin 
+mkdir -p /usr/local/bin   # harmless if directory already exists
 if [ -d /opt/devel/relish ] # It is a source distribution  
    chmod +x /opt/devel/relish/bin/*
    ln -s /opt/devel/relish/bin/relish /usr/local/bin/relish
    ln -s /opt/devel/relish/bin/rmdb /usr/local/bin/rmdb   
    ln -s /opt/devel/relish/bin/clean /usr/local/bin/clean    
    ln -s /opt/devel/relish/bin/makedist /usr/local/bin/makedist     
-   chmod -R -go+w /opt/devel/relish       
+   chmod -R -go+w /opt/devel/relish      
+elif  [ -d ~/devel/relish ] # It is a home directory binary distribution  
+   chmod +x ~/relish/bin/*   
+   ln -s ~/relish/pl/bin/relish /usr/local/bin/relish
+   ln -s ~/relish/pl/bin/rmdb /usr/local/bin/rmdb   
+   ln -s ~/relish/pl/bin/clean /usr/local/bin/clean   
+   chmod -go+w ~/relish      
+   chmod -R -go+w ~/relish/keys   
 else
    chmod +x /opt/relish/bin/*   
    ln -s /opt/relish/pl/bin/relish /usr/local/bin/relish
